4f83323014a8507f88bf377f31e94978c65edd98
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-11-22 16:32:56 -0300

    Styled the bottomm tab navigation menu

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,19 +1,32 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ScreenSearch } from "../pages/ScreenSearch";
 import { HomeStackRoutes } from "./stack.routes";
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator()
 
 export function TabRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Menu" component={HomeStackRoutes} options={{
-      headerShown: false,
-      title: 'Menu',
-    }}/>
-      <Tab.Screen name="Search" component={ScreenSearch} options={{
+    <Tab.Navigator 
+    screenOptions={{
+      tabBarActiveTintColor: '#fb8500',
+    }}
+    >
+      <Tab.Screen name="Home" component={HomeStackRoutes} 
+      options={{
+        headerShown: false,
+        title: 'Home',
+        tabBarIcon: ({color, size}) => (
+          <Ionicons name="home" size={size} color={color} />
+        ),
+      }}/>
+      <Tab.Screen name="Search" component={ScreenSearch} 
+      options={{
         headerShown: false,
         title: 'Search',
+        tabBarIcon: ({color, size}) => (
+          <Ionicons name="search" size={size} color={color} />
+        ),
       }} />
     </Tab.Navigator>
   )
